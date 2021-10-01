6ba1a0c7842101434bf72151cadd9e59db047b6a
ritik7693 <80903533+ritik7693@users.noreply.github.com>
2021-10-01 12:19:24 +0530

    5th program

---
+++ 5.py
@@ -0,0 +1,10 @@
+fruit1=input("enter name of 1 fruit: ")
+fruit2=input("enter name of 2 fruit: ")
+fruit3=input("enter name of 3 fruit: ")
+fruit4=input("enter name of 4 fruit: ")
+fruit5=input("enter name of 5 fruit: ")
+fruit6=input("enter name of 6 fruit: ")
+fruit7=input("enter name of 7 fruit: ")
+
+listoffruits= [ fruit1, fruit2, fruit3, fruit4, fruit5, fruit6, fruit7]
+print(listoffruits)
