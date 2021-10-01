e50b977245cf5ddaae95fb754ac22b1b7f488258
ritik7693 <80903533+ritik7693@users.noreply.github.com>
2021-10-01 12:22:40 +0530

    10th program

---
+++ 10.py
@@ -0,0 +1,9 @@
+mydic = {
+"maa":"mother",
+"papa":"father", 
+"bhai":"brother", 
+ "behn":"sister",
+}
+
+a= input("enter the hindi word\n")
+print("the english word for hindi word is " , mydic[a])
