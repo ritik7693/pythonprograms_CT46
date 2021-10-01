89cfc690f12a958aa9d5a2dbc0caf271fc1c62c6
ritik7693 <80903533+ritik7693@users.noreply.github.com>
2021-10-01 12:20:35 +0530

    6th program

---
+++ 6.py
@@ -0,0 +1,10 @@
+m1=int(input("enter marks of student1: "))
+m2=int(input("enter marks of student2: "))
+m3=int(input("enter marks of student3: "))
+m4=int(input("enter marks of student4: "))
+m5=int(input("enter marks of student5: "))
+m6=int(input("enter marks of student6: "))
+
+markslist= [m1, m2, m3, m4, m5, m6]
+markslist.sort()
+print(markslist)
